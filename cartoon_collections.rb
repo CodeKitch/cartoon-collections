e450bd80be30e79f1ddb09a43cb0fe228b529b4d
JaSON <masteraccounthub@gmail.com>
2020-10-27 03:30:55 -0400

    commit tree a56ed79516201b70d3047d6675e3ab73f59edaac

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index { |dwarves, index|
+  puts "#{index +1}. #{dwarves}"
+}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  veggies.map {|cap| cap.capitalize + "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.any?{|i| i.length > 4
+  }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_finder)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_finder.find{|pick|
+    cheese_types.include?(pick)
+  }
 end
